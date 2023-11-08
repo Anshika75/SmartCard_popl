b9bd2d24e8515662f8361392f7a28bec6ee7a48f
Anshika <anshikathakur596@gmail.com>
2023-11-09 02:13:44 +0530

    edit card done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Login from './pages/login'
 import Home from './pages/home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Edit from './pages/edit';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Routes>
           <Route path='/home' element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/edit" element={<Edit />} />
       </Routes>
     </BrowserRouter>
 
--- client/src/components/cardfor.jsx
+++ client/src/components/cardfor.jsx
@@ -4,7 +4,7 @@ import team from "../assets/team.png"
 export default function Cardfor() {
     return (
         <>
-            <div className="absolute top-0 left-0 bg-[#d9d9d984] w-full h-full grid place-items-center">
+            <div className="absolute top-0 left-0 bg-[#d9d9d984] w-full h-full max-h-screen grid place-items-center">
                 <div className="lg:w-[450px] bg-white rounded-lg p-8">
                     <p className='block text-black text-xl roboto font-bold text-left'>Who's this card for?</p>
                     <div className="flex flex-col lg:flex-row mt-4">
